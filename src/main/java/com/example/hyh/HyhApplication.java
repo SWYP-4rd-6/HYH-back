e4656cabd427b0309db000c9e2dcaaa862012ee6
wocks1123 <wocks1123@gmail.com>
2025-03-02 17:05:00 +0900

    init

---
+++ src/main/java/com/example/hyh/HyhApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hyh/HyhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
