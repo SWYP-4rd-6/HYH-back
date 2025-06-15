b1620e744d3555556919fa34172fadd1fef882a6
itavita08 <dudwns10000@gmail.com>
2025-06-15 19:48:53 +0900

    feat: 챙길물건 API
    - 챙길물건 API 추가

---
+++ src/main/java/com/example/hyh/pack/infra/CheckUserTestJpaRepository.java
@@ -0,0 +1,7 @@
+package com.example.hyh.pack.infra;
+
+import com.example.hyh.pack.domain.CheckUserTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckUserTestJpaRepository extends JpaRepository<CheckUserTest, String> {
+}
